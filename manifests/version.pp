401999a9202a387af71ec57065838dc1059b9dc0
Aneesh C <admn.nanonet@gmail.com>
2018-06-01 14:58:48 +0530

    Release 6.2.0

--- manifests/version.pp
+++ manifests/version.pp
@@ -22,16 +22,32 @@ class mariadb::version (
       elsif $mariadb_version == '10.1' {
         case $::operatingsystemrelease {
           /^6.*/: {
-            $header = 'MariaDB 10.1 CentOS repository list - created 2018-05-19 06:21 UTC'
-            $baseurl = 'http://yum.mariadb.org/10.1/centos6-amd64'
+            $header = 'MariaDB 10.1 RedHat repository list - created 2018-05-31 07:02 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.1/rhel6-amd64'
           }
           /^7.*/: {
-            $header = 'MariaDB 10.1 CentOS repository list - created 2018-05-19 06:17 UTC'
-            $baseurl = 'http://yum.mariadb.org/10.1/centos7-amd64'
+            $header = 'MariaDB 10.1 RedHat repository list - created 2018-05-31 07:03 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.1/rhel7-amd64'
           }
           default: {
-            $header = 'MariaDB 10.1 CentOS repository list - created 2018-05-19 06:21 UTC'
-            $baseurl = 'http://yum.mariadb.org/10.1/centos6-amd64'
+            $header = 'MariaDB 10.1 RedHat repository list - created 2018-05-31 07:02 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.1/rhel6-amd64'
+          }
+        }
+      }
+      elsif $mariadb_version == '10.0' {
+        case $::operatingsystemrelease {
+          /^6.*/: {
+            $header = 'MariaDB 10.0 RedHat repository list - created 2018-05-21 12:11 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.0/rhel6-amd64'
+          }
+          /^7.*/: {
+            $header = 'MariaDB 10.0 RedHat repository list - created 2018-05-21 11:36 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.0/rhel7-amd64'
+          }
+          default: {
+            $header = 'MariaDB 10.0 RedHat repository list - created 2018-05-21 12:11 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.0/rhel6-amd64'
           }
         }
       }
@@ -56,16 +72,32 @@ class mariadb::version (
       elsif $mariadb_version == '10.1' {
         case $::operatingsystemrelease {
           /^6.*/: {
-            $header = 'MariaDB 10.1 RedHat repository list - created 2018-05-19 06:25 UTC'
-            $baseurl = 'http://yum.mariadb.org/10.1/rhel6-amd64'
+            $header = 'MariaDB 10.1 CentOS repository list - created 2018-05-31 07:03 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.1/centos6-amd64'
           }
           /^7.*/: {
-            $header = 'MariaDB 10.1 RedHat repository list - created 2018-05-19 06:27 UTC'
-            $baseurl = 'http://yum.mariadb.org/10.1/rhel7-amd64'
+            $header = 'MariaDB 10.1 CentOS repository list - created 2018-05-31 07:05 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.1/centos7-amd64'
           }
           default: {
-            $header = 'MariaDB 10.1 RedHat repository list - created 2018-05-19 06:25 UTC'
-            $baseurl = 'http://yum.mariadb.org/10.1/rhel6-amd64'
+            $header = 'MariaDB 10.1 CentOS repository list - created 2018-05-31 07:03 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.1/centos6-amd64'
+          }
+        }
+      }
+      elsif $mariadb_version == '10.0' {
+        case $::operatingsystemrelease {
+          /^6.*/: {
+            $header = 'MariaDB 10.0 CentOS repository list - created 2018-05-21 11:40 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.0/centos6-amd64'
+          }
+          /^7.*/: {
+            $header = 'MariaDB 10.0 CentOS repository list - created 2018-05-21 11:24 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.0/centos7-amd64'
+          }
+          default: {
+            $header = 'MariaDB 10.0 CentOS repository list - created 2018-05-21 11:40 UTC'
+            $baseurl = 'http://yum.mariadb.org/10.0/centos6-amd64'
           }
         }
       }
@@ -100,6 +132,19 @@ class mariadb::version (
           }
           default: {
             $execute_repo = [ 'apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db', "add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://sgp1.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'", 'apt-get update' ]
+          }
+        }
+      }
+      elsif $mariadb_version == '10.0' {
+        case $::operatingsystemrelease {
+          /^14.*/: {
+            $execute_repo = [ 'apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db', "add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://sgp1.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main'", 'apt-get update' ]
+          }
+          /^16.*/: {
+            $execute_repo = [ 'apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8', "add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://sgp1.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu xenial main'", 'apt-get update' ]
+          }
+          default: {
+            $execute_repo = [ 'apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db', "add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://sgp1.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main'", 'apt-get update' ]
           }
         }
       }
